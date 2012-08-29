896a2e6f15498417cf45baab3149706dc0e4813a
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-08-29 13:02:54 +0200

    fixing not working gem

--- lib/omniauth/strategies/bitly.rb
+++ lib/omniauth/strategies/bitly.rb
@@ -5,34 +5,39 @@ module OmniAuth
   module Strategies
     class Bitly < OmniAuth::Strategies::OAuth2
       option :name, "bitly"
-      option :client_options,{:site=>'https://bitly.com',
-                              :authorize_url => '/oauth/authorize',
-                              :token_url => 'https://api-ssl.bit.ly/oauth/access_token'}
-      uid{ raw_info['id'] }
-      def request_phase
-        super
-      end
+      option :client_options,{:site=>'https://api-ssl.bitly.com/',
+                              :authorize_url => 'https://bitly.com/oauth/authorize',
+                              :token_url => 'https://api-ssl.bitly.com/oauth/access_token'}
+
+      option :token_params, {
+        :parse => :query
+      }
+
+      uid{ raw_info['login'] }
+
 
       info do
-        prune!({
+        {
           'login' => raw_info['login'],
           'display_name' => raw_info['display_name'],
           'full_name' => raw_info['full_name'],
           'profile_image' => raw_info['profile_image'],
-          'profile_ur'=>raw_info['profile_url'],
-          'urls' => {
-            'Bitly' => raw_info['link'],
-            'Website' => raw_info['website']
-          }
-           })
+          'profile_url'=>raw_info['profile_url'],
+          'api_key' => raw_info['apiKey']
+        }
       end
+
       extra do
         {:raw_info => raw_info}
       end
       
       def raw_info
-          @raw_info ||= access_token.get("http://api.bitly.com/").parsed['response']
+        # HACK HACK HACK
+        access_token.options[:mode] = :query
+        access_token.options[:param_name] = :access_token
+        @raw_info ||= MultiJson.encode(access_token.get('/v3/user/info').body)['data']
       end
+
     end
   end
 end
